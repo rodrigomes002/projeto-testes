e1592941fa445a161839ce342baaa71426f85704
Rodrigo Gomes Calixto da Silva <rodrigo.gomes@gmail.com>
2022-05-10 17:08:05 -0300

    adicionando melhorando os testes

--- negocios/juros.test.js
+++ negocios/juros.test.js
@@ -5,5 +5,8 @@ describe("Modulo juros", () => {
   it("calcula juros compostos", () => {
     const resultado1010 = juros.calculoJuros(1000, 0.01, 1);
     expect(resultado1010).to.equal(1010);
+
+    const resultado1020 = juros.calculoJuros(1000, 0.02, 1);
+    expect(resultado1020).to.equal(1020);
   });
 });
